2b5f1b09664c137c8c1837efa1b354982041ab24
clover_htc <clover@haituncun.com>
2016-09-08 12:21:21 +0800

    Initial Commit

---
+++ SearchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchDemo
+//
+//  Created by Clover on 9/8/16.
+//  Copyright © 2016 Clover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SearchDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SearchDemo
+//
+//  Created by Clover on 9/8/16.
+//  Copyright © 2016 Clover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
